c90011e310dcbf58a35b954fffe87cca7c00f529
SashaPolJack <lobov-aleksahdr@mail.ru>
2024-06-27 16:48:00 +0300

    fix

--- src/html5-qrcode.ts
+++ src/html5-qrcode.ts
@@ -1175,32 +1175,29 @@ export class Html5Qrcode {
         if (!this.renderedCamera) {
             return;
         }
-        const supportZoom = this.getRunningTrackCameraCapabilities().zoomFeature().isSupported()
         // There is difference in size of rendered video and one that is
         // considered by the canvas. Need to account for scaling factor.
-      
-        const elementScan:HTMLElement | HTMLVideoElement = supportZoom ? this.renderedCamera!.getSurface() : document.getElementById(this.elementId)!;
+        const elementScan = this.renderedCamera!.getSurface();
         
         const widthRatio
-            = supportZoom ? (elementScan as HTMLVideoElement).videoWidth / elementScan.clientWidth : 1;
+            =  elementScan.videoWidth / elementScan.clientWidth ;
         const heightRatio
-            = supportZoom ? (elementScan as HTMLVideoElement).videoHeight / elementScan.clientHeight: 1;
+            =  elementScan.videoHeight / elementScan.clientHeight;
 
         if (!this.qrRegion) {
             throw "qrRegion undefined when localMediaStream is ready.";
         }
 
-
         const sWidthOffset = this.qrRegion.width * widthRatio;
         const sHeightOffset = this.qrRegion.height * heightRatio;
-        const sxOffset = this.qrRegion.x * widthRatio;
-        const syOffset = this.qrRegion.y * heightRatio;
+        const sxOffset = Math.ceil(elementScan.videoWidth / 2) - Math.ceil(this.qrRegion.x / 2);
+        const syOffset =  Math.ceil(elementScan.videoHeight / 2) - Math.ceil(this.qrRegion.y / 2);
 
         // Only decode the relevant area, ignore the shaded area,
         // More reference:
         // https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/drawImage
         this.context!.drawImage(
-            elementScan as HTMLImageElement,
+            elementScan,
             /* sx= */ sxOffset,
             /* sy= */ syOffset,
             /* sWidth= */ sWidthOffset,
